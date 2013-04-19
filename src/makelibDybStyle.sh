91582bd82bc41d128cd40b9ed98c16497c809625
Ding Xuefeng <dingxf@ihep.ac.cn>
2013-04-20 03:12:58 +0800

    modify sh so that generate so at ../lib/xx.so

--- src/makelibDybStyle.sh
+++ src/makelibDybStyle.sh
@@ -1,1 +1,1 @@
-g++ DybStyle.cc -I../include -O2 -Wall -fPIC `root-config --libs` `root-config --cflags` -shared -o libDybStyle.so
+g++ DybStyle.cc -I../include -O2 -Wall -fPIC `root-config --libs` `root-config --cflags` -shared -o ../lib/libDybStyle.so
